01173c80306a76aa6a4f6a968452c44de1d9919c
hsnayrus <suryanshjain.1309@gmail.com>
2024-08-12 17:30:44 -0700

    Feat: Input and get user's choice
    Implemented functions to allow a user to enter a value and deduce their
    choice according to what they input, among rock, paper, scissor.
    Implemented it so that they cannot proceed further in the game without
    entering a valid choice

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,34 @@
-console.log("Hello World");
+const prompt = require("prompt-sync")({ sigint: true });
+
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  switch (userInput) {
+    case "rock":
+      return userInput;
+    case "paper":
+      return userInput;
+    case "scissors":
+      return userInput;
+    default:
+      console.log("Invalid choice entered");
+      return getUserInput();
+  }
+};
+
+const getUserInput = () => {
+  getUserChoice(prompt("Please enter your choice: "));
+};
+
+const showGameInstructions = () => {
+  console.log("You can choose among,");
+  console.log("Rock");
+  console.log("Paper");
+  console.log("Scissors");
+};
+
+const playGame = () => {
+  showGameInstructions();
+  getUserInput();
+};
+
+playGame();
